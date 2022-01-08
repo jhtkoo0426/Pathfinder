40ad5499dcc5b4a48c76f2078435347f753c325d
jhtkoo0426 <jhtkoo@gmail.com>
2022-01-08 23:35:19 +0000

    Added .ttf font style; Updated station model

--- homepage/views.py
+++ homepage/views.py
@@ -1,14 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from stations.models import Station
+from math import radians, sin, cos, atan2, sqrt
+
+
+def calculateDistance(lt1, lg1, lt2, lg2):
+    lat1, long1, lat2, long2 = radians(lt1), radians(lg1), radians(lt2), radians(lg2)
+    delta_lat, delta_long = lat2 - lat1, long2 - long1
+
+    a = sin(delta_lat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(delta_long / 2) ** 2
+    distance = 2 * atan2(sqrt(a), sqrt(1 - a)) * 3960.0  # In miles
+    return distance
 
 
 # Homepage View
 def home_view(request, *args, **kwargs):
+    stations = Station.objects.all()
+
+    start, end = stations.get(id=152), stations.get(id=516)
+    distance = calculateDistance(start.lat, start.lng, end.lat, end.lng)
+    results = [start.lat, start.lng, end.lat, end.lng]
+
     context = {
-        "my_text": "This is Pathfinder.",
-        "my_num": 69,
-        "some_list": ["Item 1", "Item 2", "Item 3"],
+        "db": stations,
+        "result": results,
+        "distance": distance
     }
 
-    # Return a template "home.html" instead of an HttpResponse.
-    return render(request, "app_pages/home.html", context)
+    # Return a template "pathfinder.html" instead of an HttpResponse.
+    return render(request, "app_pages/pathfinder.html", context)
--- stations/models.py
+++ stations/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Station(models.Model):
-    name = models.TextField()
+    name = models.TextField(max_length=100)
     lat = models.FloatField()
     lng = models.FloatField()
+    tubeLine = models.TextField(max_length=50, default='LINE')
